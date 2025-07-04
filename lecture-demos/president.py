372e5512a83fba716361b0da8c43a89afca39a16
Bouchra Kostet <bouchra.kostet@gmail.com>
2025-07-04 23:27:23 +0200

    done

---
+++ lecture-demos/president.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you"))
+
+if age >= 18:
+    print("You can vote")
+elif age >=21:
+    print("you can run for president")
+else:
+    print("You are too young to vote or run for president")
