2733c7004eb9e4f6230156dfdb8628aa451f0c2e
Bouchra Kostet <bouchra.kostet@gmail.com>
2025-07-04 14:55:59 +0200

    changed

--- 00-Setup/solution_00-Setup_02-Git/today.py
+++ 00-Setup/solution_00-Setup_02-Git/today.py
@@ -2,16 +2,17 @@
 This is the module docstring
 '''
 
+
 # $CHALLENGIFY_BEGIN
 def my_name_is():
     '''
     This is the function docstring
     '''
-    return "Bouchka"
+    return "ssaunier"
 
 def my_buddy_is():
     '''
     This is the function docstring
     '''
-    return "Bouchka"
+    return "ssaunier"
 # $CHALLENGIFY_END
